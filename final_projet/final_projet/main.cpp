fe2f0ce86794d400bb0d53f8ac0df9528e21336d
alexday9 <alex.dayma@gmail.com>
2019-04-21 19:13:22 +0200

    projet final
    Alex

---
+++ final_projet/final_projet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  final_projet
+//
+//  Created by dayma alexandre on 21/04/2019.
+//  Copyright © 2019 dayma alexandre. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
